9d74f1eee2dca8667a049571b4891286bfcebf12
Garrett Maury <jgmaury7921@gmail.com>
2024-12-07 21:53:42 -0500

    Project Started + Training

---
+++ main.py
@@ -0,0 +1,41 @@
+import cv2
+import torch
+import ultralytics
+import numpy as np
+from torch import cpu
+from ultralytics import YOLO
+
+def record():
+    # Open the default camera
+    cam = cv2.VideoCapture(1)
+
+    # Get the default frame width and height
+    frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('output.mp4', fourcc, 60.0, (frame_width, frame_height))
+
+    while True:
+        ret, frame = cam.read()
+
+        # Write the frame to the output file
+        out.write(frame)
+
+        # Display the captured frame
+        cv2.imshow('Camera', frame)
+
+        # Press 'q' to exit the loop
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    # Release the capture and writer objects
+    cam.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+# Create a new YOLO model from scratch
+model = YOLO("yolo11n.yaml")
+
+model.train(data = "dataset_custom.yaml", imgsz = 640, batch = 120, epochs = 200, workers = 1, device=cpu)
